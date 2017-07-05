2c34300eee386d10fe6d6370ff0a88a99041e921
Serhat Sezer <serhat.szr@gmail.com>
2017-07-05 22:55:36 +0300

    changed app initial screen as a tab bar main controller.

--- App.js
+++ App.js
@@ -1,23 +1,81 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+'use strict';
 
-export default class App extends React.Component {
-  render() {
+var React = require('react');
+var ReactNative = require('react-native');
+var {
+  StyleSheet,
+  TabBarIOS,
+  Text,
+  View,
+} = ReactNative;
+
+var MainTabbar = React.createClass({
+  statics: {
+    title: '<TabBarIOS>',
+    description: 'Tab-based navigation.',
+  },
+
+  displayName: 'MainTabbar',
+
+  getInitialState: function() {
+    return {
+      selectedTab: 'listTab',
+      presses: 0,
+    };
+  },
+
+  _renderContent: function(color: string, pageText: string, num?: number) {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <View style={[styles.tabContent, {backgroundColor: color}]}>
+        <Text style={styles.tabText}>{pageText}</Text>
+        <Text style={styles.tabText}>{num} re-renders of the {pageText}</Text>
       </View>
     );
-  }
-}
+  },
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  render: function() {
+    return (
+      <TabBarIOS
+        unselectedTintColor="white"
+        tintColor="white"
+        barTintColor="#E03161">
+
+        <TabBarIOS.Item
+          title="List"
+          selected={this.state.selectedTab === 'listTab'}
+          onPress={() => {
+            this.setState({
+              selectedTab: 'listTab',
+            });
+          }}>
+          {this._renderContent('#414A8C', 'List Tab')}
+        </TabBarIOS.Item>
+
+        <TabBarIOS.Item
+          title="Med Reminder"
+          selected={this.state.selectedTab === 'reminderTab'}
+          onPress={() => {
+            this.setState({
+              selectedTab: 'reminderTab',
+              presses: this.state.presses + 1
+            });
+          }}>
+          {this._renderContent('#21551C', 'Med Reminder', this.state.presses)}
+        </TabBarIOS.Item>
+      </TabBarIOS>
+    );
   },
 });
+
+var styles = StyleSheet.create({
+  tabContent: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  tabText: {
+    color: 'white',
+    margin: 50,
+  },
+});
+
+module.exports = MainTabbar;
